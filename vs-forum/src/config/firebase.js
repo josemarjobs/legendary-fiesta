90fcb0f8209574062dbadedcf8f66985cae5ecf9
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2022-01-18 20:44:47 -0800

    env variables

--- vs-forum/src/config/firebase.js
+++ vs-forum/src/config/firebase.js
@@ -1,8 +1,8 @@
 module.exports = {
-  apiKey: "AIzaSyBSCHeAgtVv3dF8jiBBEIlsb0ar4343Tms",
-  authDomain: "vueschoolforum-96c5b.firebaseapp.com",
-  projectId: "vueschoolforum-96c5b",
-  storageBucket: "vueschoolforum-96c5b.appspot.com",
-  messagingSenderId: "607401410941",
-  appId: "1:607401410941:web:30a96ce7307c8a308e0c14"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
